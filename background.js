5f1c01587fa6df2212fcef6bd8274707826e1349
Silviu Nechita <driver.ets@gmail.com>
2020-12-17 19:55:41 +0200

    content + other

--- background.js
+++ background.js
@@ -1,11 +1,10 @@
-var bg = document.getElementById("bg");
+         var bg = document.getElementById("bg");
          var moon = document.getElementById("moon");
          var mountain = document.getElementById("mountain");
          var road = document.getElementById("road");
          var mainpgt = document.getElementById("mainpgt");
          window.addEventListener('scroll', function(){
             var value= window.scrollY;
-
             bg.style.top = value * 0.5 + 'px';
             moon.style.left = value * 0.5 + 'px';
             mountain.style.top = value * 0.15 + 'px';
